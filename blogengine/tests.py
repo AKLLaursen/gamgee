5064741c3ae1f229505af145e7b676697bf6e2ab
Andreas Keller Leth Laursen <andreas.keller@gmail.com>
2015-12-30 12:38:55 +0100

    Tests for admin authentication has been added.

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
+from django.utils.encoding import smart_text 
 from blogengine.models import Post
 
 # Test for blogpost creation
@@ -33,3 +34,59 @@ class PostTest(TestCase):
 		self.assertEquals(onlyPost.pubDate.hour, post.pubDate.hour)
 		self.assertEquals(onlyPost.pubDate.minute, post.pubDate.minute)
 		self.assertEquals(onlyPost.pubDate.second, post.pubDate.second)
+
+# Test login on the admin page
+class AdminTest(LiveServerTestCase):
+
+	# Load fixtures
+	fixtures = ['users.json']
+
+	def testLogIn(self):
+
+		# Create the client
+		cl = Client()
+
+		# Get the login page, note that redirection has to be given explicitly, otherwise a 302 response code is given.
+		response = cl.get('/admin/', follow = True)
+
+		# Check the response code
+		self.assertEquals(response.status_code, 200)
+
+		# Check the response text, note that in python 3 byte and str is two different data types. Convertion is easily handled using smart_text.
+		self.assertTrue('Log in' in smart_text(response.content))
+
+		# Log the user in
+		cl.login(username = 'testuser', password = 'testuserpass')
+
+		# Check the response code again
+		response = cl.get('/admin/', follow = True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check the response text again
+		self.assertTrue('Log out' in smart_text(response.content))
+
+	def testLogOut(self):
+
+		# Create the client
+		cl = Client()
+
+		# Log the user in
+		cl.login(username = 'testuser', password = 'testuserpass')
+
+		# Check the response code
+		response = cl.get('/admin/', follow = True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check the response text
+		self.assertTrue('Log out' in smart_text(response.content))
+
+		# Log out
+		cl.logout()
+
+		# Check the response code again
+		response = cl.get('/admin/', follow = True)
+		self.assertEquals(response.status_code, 200)
+
+		# Check the response text again
+		self.assertTrue('Log in' in smart_text(response.content))
+		
