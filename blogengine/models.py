2a5820eecf990bbfac0dc4ff35abc6e94695cb2b
Andreas Keller Leth Laursen <andreas.keller@gmail.com>
2016-01-09 14:24:53 +0100

    Fixed direct url bug. (Python 2 -> Python 3 handling.) Further made the direct url pattern to be .../year/month/day/slug.

--- blogengine/models.py
+++ blogengine/models.py
@@ -3,13 +3,15 @@ from django.db import models
 # Blogpost creation
 class Post(models.Model):
 
+	# Model specifications
 	title = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField()
 	text = models.TextField()
 	slug = models.SlugField(max_length = 40, unique = True)
 
+	# Define url for each post. (Possibly change this to have 0 in front of single number elements.)
 	def get_abs_url(self):
-		return '/%s/%s/%s/' % (self.pub_date.year, self.pub_date.month, self.slug)
+		return '/{0}/{1}/{2}/{3}/'.format(self.pub_date.year, self.pub_date.month, self.pub_date.day, self.slug)
 
 	def __unicode__(self):
 		return self.title
--- blogengine/urls.py
+++ blogengine/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 			)),
 
 		# Individual posts
-		url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(model = Post,
+		url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<pub_date__day>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(model = Post,
 			)),
 ]
